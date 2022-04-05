f4c408acc2489349b10b6d6cc264d04bfbab5111
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2022-04-05 16:37:29 -0300

    feat: create constants

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,11 +1,13 @@
+import { INCREASE_COUNTER, DECREASE_COUNTER } from '../../constants'
+
 export const increase = () => {
     return {
-        type: 'INCREASE_COUNTER'
+        type: INCREASE_COUNTER
     }
 }
 
 export const decrease = () => {
     return {
-        type: 'DECREASE_COUNTER'
+        type: DECREASE_COUNTER
     }
 }
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,11 +1,13 @@
+import { INCREASE_COUNTER, DECREASE_COUNTER } from '../../constants'
+
 const counter = ( state = {counter: 0} , action ) => {
     switch(action.type) {
-        case 'INCREASE_COUNTER':
+        case INCREASE_COUNTER:
             return {
                 ...counter,
                 counter: state.counter + 1
             }
-        case 'DECREASE_COUNTER': 
+        case DECREASE_COUNTER: 
             return {
                 ...counter, 
                 counter: state.counter - 1
